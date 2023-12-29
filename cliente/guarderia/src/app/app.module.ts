0ea456f7313d56084ce5253dd47a188749c200af
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-12-29 19:24:50 -0300

    Added service worker

--- cliente/guarderia/src/app/app.module.ts
+++ cliente/guarderia/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -75,6 +75,7 @@ import {
   ErrorCrearEntidadExistenteComponent
 } from './error-crear-entidad-existente/error-crear-entidad-existente.component';
 import { LicensesComponent } from './licenses/licenses.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 export const FORMATOS_FECHAS = {
   parse: {
@@ -145,6 +146,12 @@ export const FORMATOS_FECHAS = {
     MatNativeDateModule,
     MatCheckboxModule,
     KeyValuePipe,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
